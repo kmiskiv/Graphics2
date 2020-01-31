0ed4221fffd478e6ddcee8c0c34ad7bf7a6c2603
Kayla <kayla.miskiv@mymail.champlain.edu>
2020-01-31 12:16:27 -0500

    Copied over Phong from Lab 2

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawPhong_multi_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawPhong_multi_fs4x.glsl
@@ -31,10 +31,78 @@
 //	4) implement Phong shading model
 //	Note: test all data and inbound values before using them!
 
+
+//3) declare inbound varying data
+in vec4 vPassNormal; 
+in vec3 vPassLight; 
+in vec3 vPassView; 
+in vec2 vPassTexcoord; 
+in vec4 viewPos; 
+
+ //2) declare uniform variables for lights
+ uniform int uLightCt; 
+ uniform float uLightSz[4]; 
+ uniform float uLightSzInvSq[4]; 
+ uniform vec4 uLightPos[4]; 
+ uniform vec4 uLightCol[4];
+
+
+// 1) declare uniform variable for textures 
+//dm for diffuse map 
+uniform sampler2D uTex_dm; 
+//sm at the end because it's a specular map; 
+uniform sampler2D uTex_sm; 
+
 out vec4 rtFragColor;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE GREEN
-	rtFragColor = vec4(0.0, 1.0, 0.0, 1.0);
+	//4) implement Phong shading model
+
+	//grab sample from texture 
+	vec4 diffuseSample = texture(uTex_dm, vPassTexcoord); 
+	vec4 specularSample = texture(uTex_sm, vPassTexcoord);
+	 
+	vec3 N = normalize(vPassNormal.xyz); 
+	 
+	vec3 diffuseTotal = vec3(0.0); 
+	vec3 specularTotal = vec3(0.0); 
+
+
+	//the light loop
+	for (int i = 0; i < uLightCt; i++)
+	{
+		vec3 L = normalize(uLightPos[i].xyz - viewPos.xyz);
+		float diffuse = dot (N, L);	
+		diffuse = max(diffuse, 0.0);
+		diffuseTotal += diffuse * uLightCol[i].rgb;
+
+		//calculate the reflection vector, diffuse constant, specular constant 
+
+
+		//formula = 2( N dot L) N - L
+		vec3 R = (diffuse + diffuse) * (N - L); 
+
+		//SPECULAR 
+		vec3 V = normalize(vPassView); 
+		
+		
+		float specular = dot (V, R); 
+
+
+		//clamp specular to get rid of negative numbers 
+		specular = max(0.0, specular); 
+
+		specularTotal += specular * uLightCol[i].rgb; 
+
+
+	//put together in a similar fashion as you put together diffuse 
+	// 	PHONG
+	//phong is the sum of diffuse, specular and ambient components. 
+	vec3 Phong = (diffuseTotal * diffuseSample.rgb) 
+				+ (specularTotal * specularSample.rgb)
+				+  vec3(0.01, 0.0, 0.02); 
+	
+	rtFragColor = vec4(Phong, diffuseSample.a); 
+
 }
