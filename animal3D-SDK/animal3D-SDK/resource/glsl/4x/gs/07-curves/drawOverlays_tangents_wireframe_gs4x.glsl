65a06919a526b4736635b1036a7a68ee09376d0f
kmiskiv <kayla.miskiv@mymail.champlain.edu>
2020-02-25 10:42:12 -0500

    Class notes

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/gs/07-curves/drawOverlays_tangents_wireframe_gs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/gs/07-curves/drawOverlays_tangents_wireframe_gs4x.glsl
@@ -41,7 +41,46 @@
 //	6) draw tangent bases
 //	7) draw wireframe
 
+//1) 
+layout (triangles) in; 
+
+
+//2)
+in vbVertexData{
+	mat4 vTangentBasis_view; 
+	vec4 vTexcoord_atlas; 
+	flat int vVertexID, vInstanceID, vModeID; 
+
+
+} vVertexData[]; //3 elements because traingle 
+
+//4)
+layout(line_strip, max_vertices = MAX_VERTICES) out; 
+
+
+//5)
+out vec4 vColor; 
+
+void drawWireframe()
+{
+	vColor = vec4(1.0, 0.5, 0.0, 1.0); 
+
+	gl_Position = gl_in[0].gl_Position;
+	EmitVertex(); 
+	gl_Position = gl_in[1].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[2].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[3].gl_Position;
+	EmitVertex();
+	EndPrimitive(); 
+}
+
+
 void main()
 {
+
+drawWireframe(); 
+
 	
 }
